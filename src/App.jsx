4d44c05a9ccf2c8f9e85a03ac214257f5440e81c
iambestday <behrouz.asghari@gmail.com>
2024-04-30 09:48:33 +0330

    refactor with BEM Principles

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ function App() {
   return (
     <div className="main-wrapper">
       <header className="header">
-        <h1>خدمات رادکام</h1>
-        <h2>محصولات ارائه شده توسط رادکام</h2>
+        <h1 className="header__title">خدمات رادکام</h1>
+        <h2 className="header__subtitle">محصولات ارائه شده توسط رادکام</h2>
       </header>
       <div className="container">
         {data.map((item, index) => (
           <div key={index} className="items">
-            <div className="overlay-rectangle-left"/>
+            <div className="overlay-rectangle-left"></div>
             <img
               src={
                 item.image === "windows"
@@ -22,15 +22,15 @@ function App() {
                   : domain
               }
               alt={item.alt}
+              className="items__image"
             />
-            <h2>{item.title}</h2>
-            <p className="description">{item.description}</p>
-            <button>
-              <div>
-              {item.buttonText}</div>
-              <img src={arrow} alt={"arrow"} />
+            <h2 className="items__title">{item.title}</h2>
+            <p className="items__description">{item.description}</p>
+            <button className="items__button">
+              <div>{item.buttonText}</div>
+              <img src={arrow} alt="arrow" className="items__button-icon" />
             </button>
-            <div className="overlay-rectangle-right"/>
+            <div className="overlay-rectangle-right"></div>
           </div>
         ))}
       </div>
